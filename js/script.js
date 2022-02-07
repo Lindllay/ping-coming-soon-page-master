7eb7d85ea9d72d52e0ea9d1f5d50a3be3c8da88d
Lindllay <93735183+Lindllay@users.noreply.github.com>
2022-02-07 18:45:20 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ document.querySelector('#button').addEventListener('click', function(){
     if(!input){
         document.querySelector('.error').style.display ="block";
         document.querySelector('.error').textContent = "Please enter your email"
+        document.querySelector('#email').style.borderColor ="red";
     }
     else if(!input.match(pattern)){
         document.querySelector('.error').style.display ="block";
         document.querySelector('.error').textContent = "Please provide a valid email address"
+        document.querySelector('#email').style.borderColor ="red";
     }
     else{
         document.querySelector('.error').style.display ="none";
+        document.querySelector('#email').style.borderColor ="grey";
     }
 })
